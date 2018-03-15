81ccf9eb4da24f11ec86a7a22a06c91e27b99a16
mutongliwansui <1218250915@qq.com>
2018-03-15 22:51:22 +0800

    Initial commit

---
+++ src/main/java/com/mtl/dashboard/SpringCloudDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.mtl.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class SpringCloudDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDashboardApplication.class, args);
+	}
+}
